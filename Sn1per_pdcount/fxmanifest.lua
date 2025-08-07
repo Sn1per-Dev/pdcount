fbd9900b064a32915d68d7e0c34cee153775ffb6
Sn1per <139951625+Sn1per-Dev@users.noreply.github.com>
2025-08-07 10:41:05 +0200

    Add files via upload

---
+++ Sn1per_pdcount/fxmanifest.lua
@@ -0,0 +1,19 @@
+-- Author: Sn1per
+-- Copyright (c) 2025 Sn1per. All rights reserved.
+-- This script is protected by copyright law. Unauthorized copying, modification, distribution, or use of this script or its components is strictly prohibited.
+-- The folder name must remain 'Sn1per_pdcount'. Renaming or modifying the folder name will result in the script not working.
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Sn1per'
+
+
+shared_script 'config.lua'
+server_script 'server.lua'
+client_script 'client.lua'
+
+dependencies {
+    'es_extended',
+    'chat'
+}
